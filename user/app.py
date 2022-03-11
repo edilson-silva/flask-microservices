e679db944a682a359495e7c7e48b7915fd7f4edd
Edilson Silva <eddysilva1994@gmail.com>
2022-03-11 02:01:17 -0300

    Feat: Added app configuration to initialize database and generate migrations

--- user/app.py
+++ user/app.py
@@ -1,15 +1,24 @@
-import models
+from os import environ
 
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 from flask import Flask
+from flask_migrate import Migrate
+
+import models
 from routes import user_blueprint
 
+load_dotenv(find_dotenv())
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'QBzTq9qaCXwbxV0StBehew'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS').lower() in ("true", "1")
 
 models.init_app(app)
 app.register_blueprint(user_blueprint)
 
+migrate = Migrate(app, models.db)
 
 if __name__ == '__main__':
     app.run(debug=True)
