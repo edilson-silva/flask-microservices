fb885565377023dba899cf67998a4d112679caa5
Edilson Silva <eddysilva1994@gmail.com>
2022-03-11 00:19:27 -0300

    Feat: Added user model and make api and db integration.

--- user/app.py
+++ user/app.py
@@ -4,6 +4,10 @@ from flask import Flask
 from routes import user_blueprint
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'QBzTq9qaCXwbxV0StBehew'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+
+models.init_app(app)
 app.register_blueprint(user_blueprint)
 
 
