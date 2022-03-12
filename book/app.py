5b4819218bdc17de635f8064ebc8f4ca50650d75
Edilson Silva <eddysilva1994@gmail.com>
2022-03-12 00:50:32 -0300

    Feat: Add book microservice project base app

---
+++ book/app.py
@@ -0,0 +1,15 @@
+from os import environ
+
+from dotenv import find_dotenv
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS').lower() in ("true", "1")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
