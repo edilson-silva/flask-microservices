fd0c5754ac3a49c51d334f2752582c412c81651e
Edilson Silva <eddysilva1994@gmail.com>
2022-03-14 23:14:56 -0300

    Feat: Added method to get user from request headers in order microservice

--- order/routes.py
+++ order/routes.py
@@ -1,5 +1,6 @@
 from os import environ
 
+import requests
 from flask import Blueprint
 from flask import jsonify
 
@@ -8,6 +9,20 @@ from models import Order
 order_blueprint = Blueprint('order_api_routes', __name__, url_prefix='/api/order')
 
 USER_API = environ.get('USER_API')
+
+
+def get_user(api_key):
+    headers = {
+        'Authorization': api_key
+    }
+
+    response = requests.get(USER_API, headers=headers)
+
+    if not response.ok:
+        return {'message': 'User nor authorized'}
+
+    user = response.json()
+    return user
 
 
 @order_blueprint.route('/all', methods=['GET'])
