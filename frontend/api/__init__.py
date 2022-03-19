2b6929a6e54813410b0439f4ad598ad370d418d6
Edilson Silva <eddysilva1994@gmail.com>
2022-03-19 00:41:48 -0300

    Feature: Added user api communicate, specifying the enpoints

---
+++ frontend/api/__init__.py
@@ -0,0 +1,5 @@
+from os import environ
+
+USER_API_URL = environ.get('USER_API_URL')
+BOOK_API_URL = environ.get('BOOK_API_URL')
+ORDER_API_URL = environ.get('ORDER_API_URL')
---
+++ frontend/api/user_api.py
@@ -0,0 +1,55 @@
+import requests
+from flask import session
+
+from . import USER_API_URL
+
+
+class UseClient:
+
+    @staticmethod
+    def login(form):
+        api_key = None
+        payload = {
+            'username': form.username.data,
+            'password': form.password.data
+        }
+
+        url = f'{USER_API_URL}login'
+        response = requests.post(url, data=payload)
+
+        if response:
+            api_key = response.json().get('api_key')
+
+        return api_key
+
+    @staticmethod
+    def get_user():
+        headers = {
+            'Authorization': session.get('user_api_key')
+        }
+
+        response = requests.get(USER_API_URL, headers=headers)
+        user = response.json()
+        return user
+
+    @staticmethod
+    def create_user(form):
+        user = None
+        payload = {
+            'username': form.username.data,
+            'password': form.password.data
+        }
+
+        url = f'{USER_API_URL}create'
+        response = requests.post(url, data=payload)
+
+        if response:
+            user = response.json()
+
+        return user
+
+    @staticmethod
+    def user_exists(username):
+        url = f'{USER_API_URL}{username}/exists'
+        response = requests.get(url)
+        return response.ok
